ab4a070d3a97f5d6f8b8de423668b2bc26b429c9
Kas Buunk <kasbuunk@icloud.com>
2025-02-05 20:51:27 +0100

    feat: define and

--- test/Spec.hs
+++ test/Spec.hs
@@ -25,9 +25,21 @@ testNot = and [
 	not_ Zero == One
 	]
 
+testAnd :: Bool
+testAnd = and [
+	and_ One One == One,
+	and_ One Zero == Zero,
+	and_ Zero One == Zero,
+	and_ Zero Zero == Zero
+	]
+
 nand :: Bit -> Bit -> Bit
 nand One One = Zero
 nand _ _ = One
 
 not_ :: Bit -> Bit
 not_ b = nand b b
+
+and_ :: Bit -> Bit -> Bit
+and_ One One = One
+and_ _ _ = Zero
