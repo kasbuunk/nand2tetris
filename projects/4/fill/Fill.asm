a353eaede858aa67fba9db775cd1cf5bfe2cbf71
Kas Buunk <kasbuunk@icloud.com>
2025-03-12 19:06:46 +0100

    feat: finish project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -7,4 +7,73 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(CLEARED)
+	// Fill if key is pressed.
+	@KBD 
+	D=M
+	@FILL
+	D;JNE
+	@CLEARED
+	0;JMP
+(CLEAR)
+	// Make all pixels white.
+	@SCREEN
+	D=A
+	@PIXEL
+	M=D
+	@CLEARING
+	0;JMP
+(CLEARING)
+	// Done if PIXEL reaches KBD, i.e. end of screen.
+	@PIXEL
+	D=M
+	@KBD
+	D=A-D
+	@CLEARED
+	D;JEQ
+	// Whiten current PIXEL.
+	@PIXEL
+	A=M
+	M=0
+	// Increment PIXEL.
+	D=A+1
+	@PIXEL
+	M=D
+	// Loop.
+	@CLEARING
+	0;JMP
+(FILL)
+	// Make all pixels black.
+	@SCREEN
+	D=A
+	@PIXEL
+	M=D
+	@FILLING
+	0;JMP
+(FILLING)
+	// Done if PIXEL reaches KBD, i.e. end of screen.
+	@PIXEL
+	D=M
+	@KBD
+	D=A-D
+	@FILLED
+	D;JEQ
+	// Blacken current PIXEL.
+	@PIXEL
+	A=M
+	M=-1
+	// Increment PIXEL.
+	D=A+1
+	@PIXEL
+	M=D
+	// Loop.
+	@FILLING
+	0;JMP
+(FILLED)
+	// Clear if no key is pressed.
+	@KBD 
+	D=M
+	@CLEAR
+	D;JEQ
+	@FILLED
+	0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,4 +6,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+(LOOP)
+	// D = RAM[0]
+	@R0
+	D=M
+	// Stop if R0 is 0.
+	@END
+	D;JEQ
+	// Decrement R0
+	@R0
+	M=M-1
+	// RAM[2] = RAM[2] + RAM[1].
+	@R1
+	D=M
+	@R3
+	M=D+M
+	// Back to start of loop.
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
